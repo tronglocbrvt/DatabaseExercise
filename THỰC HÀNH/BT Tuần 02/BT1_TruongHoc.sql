1655820e9eb4e3c3ba3e07941e033a30f61e9766
Trương Trọng Lộc <49932238+tronglocbrvt@users.noreply.github.com>
2020-05-12 12:16:00 +0700

    update BT TH tuần 2

---
+++ THỰC HÀNH/BT Tuần 02/BT1_TruongHoc.sql
@@ -0,0 +1,70 @@
+﻿create database TruongHoc
+go
+use TruongHoc
+go
+create table Truong
+(
+	MaTruong char(6),
+	TenTruong nvarchar(40)
+	primary key (MaTruong)	
+)
+go
+create table DayNha
+(
+	MaDay char(3),
+	MaTruong char(6),
+	TenDay nvarchar(30)
+	primary key (MaDay, MaTruong),
+	foreign key (MaTruong) references Truong(MaTruong)
+)
+go
+create table Phong
+(
+	MaPhong char(5),
+	MaDay char(3),
+	MaTruong char(6),
+	MaPhongQuanLy char(5)
+	primary key (MaPhong, MaDay, MaTruong),
+	foreign key (MaDay, MaTruong) references DayNha(MaDay, MaTruong),
+	foreign key (MaPhongQuanLy, MaDay, MaTruong) references Phong(MaPhong, MaDay, MaTruong)
+)
+go
+insert into Truong(MaTruong, TenTruong) values ('HCMUS', N'Khoa Học Tự Nhiên')
+insert into Truong(MaTruong, TenTruong) values ('HCMUT', N'Bách Khoa')
+insert into Truong(MaTruong, TenTruong) values ('HCMUEL', N'Kinh Tế Luật')
+insert into Truong(MaTruong, TenTruong) values ('HCMUIT', N'Công nghệ thông tin')
+
+insert into DayNha(MaDay, MaTruong, TenDay) values ('A', 'HCMUS', N'Dãy nhà A')
+insert into DayNha(MaDay, MaTruong, TenDay) values ('A', 'HCMUT', N'Dãy nhà A')
+insert into DayNha(MaDay, MaTruong, TenDay) values ('B', 'HCMUIT', N'Dãy nhà B')
+insert into DayNha(MaDay, MaTruong, TenDay) values ('B', 'HCMUEL', N'Dãy nhà B')
+
+insert into Phong(MaPhong, MaDay, MaTruong) values ('001', 'A', N'HCMUS')
+insert into Phong(MaPhong, MaDay, MaTruong) values ('002', 'A', N'HCMUS')
+insert into Phong(MaPhong, MaDay, MaTruong) values ('003', 'A', N'HCMUS')
+insert into Phong(MaPhong, MaDay, MaTruong) values ('001', 'A', N'HCMUT')
+insert into Phong(MaPhong, MaDay, MaTruong) values ('002', 'A', N'HCMUT')
+insert into Phong(MaPhong, MaDay, MaTruong) values ('003', 'A', N'HCMUT')
+insert into Phong(MaPhong, MaDay, MaTruong) values ('001', 'B', N'HCMUIT')
+insert into Phong(MaPhong, MaDay, MaTruong) values ('002', 'B', N'HCMUIT')
+insert into Phong(MaPhong, MaDay, MaTruong) values ('003', 'B', N'HCMUIT')
+insert into Phong(MaPhong, MaDay, MaTruong) values ('001', 'B', N'HCMUEL')
+insert into Phong(MaPhong, MaDay, MaTruong) values ('002', 'B', N'HCMUEL')
+insert into Phong(MaPhong, MaDay, MaTruong) values ('003', 'B', N'HCMUEL')
+
+update Phong set MaPhongQuanLy = '003' where MaPhong = '001' and MaDay = 'A' and MaTruong = 'HCMUS'
+update Phong set MaPhongQuanLy = '002' where MaPhong = '003' and MaDay = 'A' and MaTruong = 'HCMUS'
+update Phong set MaPhongQuanLy = '001' where MaPhong = '002' and MaDay = 'A' and MaTruong = 'HCMUS'
+
+update Phong set MaPhongQuanLy = '002' where MaPhong = '001' and MaDay = 'A' and MaTruong = 'HCMUT'
+update Phong set MaPhongQuanLy = '001' where MaPhong = '003' and MaDay = 'A' and MaTruong = 'HCMUT'
+update Phong set MaPhongQuanLy = '003' where MaPhong = '002' and MaDay = 'A' and MaTruong = 'HCMUT'
+
+update Phong set MaPhongQuanLy = '001' where MaPhong = '001' and MaDay = 'B' and MaTruong = 'HCMUEL'
+update Phong set MaPhongQuanLy = '002' where MaPhong = '003' and MaDay = 'B' and MaTruong = 'HCMUEL'
+update Phong set MaPhongQuanLy = '003' where MaPhong = '002' and MaDay = 'B' and MaTruong = 'HCMUEL'
+
+update Phong set MaPhongQuanLy = '003' where MaPhong = '001' and MaDay = 'B' and MaTruong = 'HCMUIT'
+update Phong set MaPhongQuanLy = '002' where MaPhong = '002' and MaDay = 'B' and MaTruong = 'HCMUIT'
+update Phong set MaPhongQuanLy = '001' where MaPhong = '003' and MaDay = 'B' and MaTruong = 'HCMUIT'
+
---
+++ THỰC HÀNH/BT Tuần 02/BT2_PhongMay.sql
@@ -0,0 +1,65 @@
+﻿create database PHONGMAY
+go
+use PHONGMAY
+go
+create table PhongMay
+(
+	MaPhong char(6),
+	TenPhong nvarchar(40),
+	MayChu char(6),
+	MaNVQL char(6)
+	primary key (MaPhong)	
+)
+go
+create table MayTinh
+(
+	MaMT char(6),
+	TenMT nvarchar(40),
+	MaPM char(6),
+	TinhTrang BIT
+	primary key (MaMT, MaPM)	
+)
+
+go
+create table NhanVien
+(
+	MaNV char(6),
+	TenNV nvarchar(40) UNIQUE,
+	MaNVQL char(6),
+	PHAI nvarchar(3) check (PHAI IN ('Nam', N'Nữ'))
+	primary key (MaNV)	
+)
+
+go
+alter table PhongMay add constraint FK_MayChu_MaPhong foreign key (MayChu, MaPhong) references MayTinh(MaMT, MaPM)
+alter table PhongMay add constraint FK_MaNVQL foreign key (MANVQL) references NhanVien(MaNV)
+alter table NhanVien add constraint FK_MaNVQLNV foreign key (MANVQL) references NhanVien(MaNV)
+alter table MayTinh add constraint FK_MaPM foreign key (MAPM) references PhongMay(MaPhong)
+
+go
+insert into NhanVien(MaNV, TenNV, PHAI) values ('001', N'Nguyễn Văn A', 'Nam')
+insert into NhanVien(MaNV, TenNV, PHAI) values ('002', N'Nguyễn Thị Hồng', N'Nữ')
+insert into NhanVien(MaNV, TenNV, PHAI) values ('003', N'Nguyễn Xuân Bảo', 'Nam')
+insert into NhanVien(MaNV, TenNV, PHAI) values ('004', N'Nguyễn Thị Hằng', N'Nữ')
+
+update NhanVien set MaNVQL = '002' where MaNV = '001'
+update NhanVien set MaNVQL = '003' where MaNV = '002'
+update NhanVien set MaNVQL = '004' where MaNV = '003'
+update NhanVien set MaNVQL = '001' where MaNV = '004'
+
+insert into PhongMay(MaPhong, TenPhong, MaNVQL) values ('CNPM', N'Công nghệ Phần Mềm', '001')
+insert into PhongMay(MaPhong, TenPhong, MaNVQL) values ('HTTT', N'Hệ Thống Thông Tin', '004')
+insert into PhongMay(MaPhong, TenPhong, MaNVQL) values ('KHMT', N'Khoa Học Máy Tính', '002')
+insert into PhongMay(MaPhong, TenPhong, MaNVQL) values ('MMT', N'Mạng Máy Tính và Viễn Thông', '003')
+
+insert into MayTinh(MaMT, TenMT, MaPM, TinhTrang) values ('A', N'Máy A', 'CNPM', 1)
+insert into MayTinh(MaMT, TenMT, MaPM, TinhTrang) values ('B', N'Máy B', 'CNPM', 0)
+insert into MayTinh(MaMT, TenMT, MaPM, TinhTrang) values ('A', N'Máy A', 'MMT', 1)
+insert into MayTinh(MaMT, TenMT, MaPM, TinhTrang) values ('A', N'Máy A', 'HTTT', 1)
+insert into MayTinh(MaMT, TenMT, MaPM, TinhTrang) values ('B', N'Máy B', 'KHMT', 1)
+insert into MayTinh(MaMT, TenMT, MaPM, TinhTrang) values ('A', N'Máy A', 'KHMT', 0)
+
+update PhongMay set MayChu = 'A' where MaPhong = 'CNPM'
+update PhongMay set MayChu = 'A' where MaPhong = 'HTTT'
+update PhongMay set MayChu = 'B' where MaPhong = 'KHMT'
+update PhongMay set MayChu = 'A' where MaPhong = 'MMT'
